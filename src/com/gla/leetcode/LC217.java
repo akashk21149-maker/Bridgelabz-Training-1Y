36c8e6648841735dfb1bfe079d208693af118871
akashk21149-maker <akashk21149@gmail.com>
2026-03-30 20:48:27 +0530

    first commit by Akash Kumar

---
+++ src/com/gla/leetcode/LC217.java
@@ -0,0 +1,14 @@
+package com.gla.leetcode;
+
+public class LC217 {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args);
+}
